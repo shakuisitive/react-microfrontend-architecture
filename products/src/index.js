73087ef85b9e958b2b996154b19d70c31c677a42
Shakir <shakirkhan72727@gmail.com>
2025-08-24 18:17:26 +0500

    change var name

--- products/src/index.js
+++ products/src/index.js
@@ -3,7 +3,8 @@ import faker from "faker";
 let products = "";
 
 for (let i = 0; i < 10; i++) {
-  let name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  let productName = faker.commerce.productName();
+  products += `<div>${productName}</div>`;
 }
+
 document.querySelector("#dev-products").innerHTML = products;
