8d4b01d2d01dda0a8e41801bee98f08d0bb30262
Shakir <shakirkhan72727@gmail.com>
2025-08-24 23:31:25 +0500

    share faker library to multiple apps + lazy import

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from "faker";
-
-let cartText = `<div>You have ${faker.random.number()} products in your cart.</div>`;
-
-document.querySelector("#cart-dev").innerHTML = cartText;
+import("./bootstrap");
--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-import faker from "faker";
-
-let products = "";
-
-for (let i = 0; i < 10; i++) {
-  let productName = faker.commerce.productName();
-  products += `<div>${productName}</div>`;
-}
-
-document.querySelector("#dev-products").innerHTML = products;
+import("./bootstrap");
